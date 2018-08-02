1af9eac8c4f2b0f02928d47f1a6f1399a9a06491
ken woodruff <kwoodruf@adobe.com>
2018-08-01 21:44:33 -0700

    Integrated BikeDeedServer with BikeDeed.  When web3 is not detected, refer the user to the BikeDeedServer.

--- app/javascript/bikeappvue.js
+++ app/javascript/bikeappvue.js
@@ -50,6 +50,8 @@ var app = new Vue({
         mybikes: [],
         bikelist: [],
         manufacturers: [],
+        web3Enabled: false,
+        networkLabel: '',
         // display controls
         search: '',
         showDetailsModal: false,
@@ -83,11 +85,13 @@ var app = new Vue({
       mounted:function(){
         console.log("mounted...");
         this.initWeb3();
-        this.initAccounts();
-        this.initContract();
-        this.loadAllBikes();
-        this.initManufacturers();
-        //alert("performed mounted functions");
+        if (this.web3Enabled == true) {
+          this.initAccounts();
+          this.initContract();
+          this.loadAllBikes();
+          this.initManufacturers();
+          //alert("performed mounted functions");
+        }
       },
       beforeUpdate:function(){
         console.log("beforeUpdate...");
@@ -103,14 +107,40 @@ var app = new Vue({
           // Checking if Web3 has been injected by the browser (Mist/MetaMask)
           let self = this;
           if (typeof web3 !== 'undefined') {
-            //console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 BikeDeeds, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
             console.warn("Using injected web3")
             // Use Mist/MetaMask's provider
             window.web3 = new Web3(web3.currentProvider);
+
+            var networkId = web3.version.network;
+
+            console.log('networkId: ' + networkId);
+
+            switch (networkId) {
+            case "1":
+              this.networkLabel = "BikeDeed (Mainnet)";
+              break;
+            case "2":
+              this.networkLabel = "You are on the Morden Network - Please switch to Mainnet";
+              break;
+            case "3":
+              this.networkLabel = "You are on the Ropsten Network - Please switch to Mainnet";
+              break;
+            case "4":
+              this.networkLabel = "You are on the Rinkeby Network - Please switch to Mainnet";
+              break;
+            case "42":
+              this.networkLabel = "You are on the Kovan Network - Please switch to Mainnet";
+              break;
+            default:
+              this.networkLabel = "BikeDeed";
+            }
+            this.web3Enabled = true;
           } else {
-            console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+            console.warn("No web3 detected. .");
             // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-            window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+            this.web3Enabled = false;
+            this.networkLabel = "Ethereum is not enabled. Go to <a href=/bikes>read-only site.</a>";
+            //window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
           }
         },
         initAccounts:function(){
